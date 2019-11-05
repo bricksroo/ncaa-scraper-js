2a9ddac66167ec9cc1ef7c54e2bed4106b38166b
Mason Hahn <mhahn@weedmaps.com>
2019-11-05 08:38:41 -0800

    update for 2019-2020 season

--- ranks.js
+++ ranks.js
@@ -1,21 +1,21 @@
-const axios = require('axios')
-const argv = require('minimist')(process.argv.slice(2))
+const axios = require("axios");
+const argv = require("minimist")(process.argv.slice(2));
 
 const getArg = (short, long) => {
-  if (argv[short] != null) return argv[short]
-  if (argv[long] != null) return argv[long]
-  return null
-}
-const weekArg = getArg('w', 'week')
+  if (argv[short] != null) return argv[short];
+  if (argv[long] != null) return argv[long];
+  return null;
+};
+const weekArg = getArg("w", "week");
 
 const get = async week => {
   try {
     const req = await axios.get(
-      `http://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/rankings?year=2018&poll=1&week=${week}&seasontype=2`
-    )
+      `http://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/rankings?year=2019&poll=1&week=${week}&seasontype=2`
+    );
     const { ranks: top25, others } = req.data.rankings.find(
-      ranking => ranking.type === 'ap'
-    )
+      ranking => ranking.type === "ap"
+    );
 
     const scrapeRanks = toScrape => {
       return toScrape.reduce((prev, curr) => {
@@ -25,18 +25,18 @@ const get = async week => {
           firstPlaceVotes,
           recordSummary,
           previous
-        } = curr
+        } = curr;
         return `${prev}\n ${teamName},${points},${
-          firstPlaceVotes === 0 ? ' ' : "'(" + firstPlaceVotes + ')'
-        },${recordSummary === '0-0' ? ' ' : recordSummary},${
-          previous === 0 ? 'NR' : previous
-        }`
-      }, '')
-    }
+          firstPlaceVotes === 0 ? " " : "'(" + firstPlaceVotes + ")"
+        },${recordSummary === "0-0" ? " " : recordSummary},${
+          previous === 0 ? "NR" : previous
+        }`;
+      }, "");
+    };
 
-    console.log(`${scrapeRanks(top25)}\n${scrapeRanks(others)}`)
+    console.log(`${scrapeRanks(top25)}\n${scrapeRanks(others)}`);
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
-}
-get(weekArg)
+};
+get(weekArg);
--- scores.js
+++ scores.js
@@ -1,130 +1,134 @@
-const axios = require('axios')
-const format = require('date-fns/format')
-const getTime = require('date-fns/get_time')
-const argv = require('minimist')(process.argv.slice(2))
+const axios = require("axios");
+const format = require("date-fns/format");
+const getTime = require("date-fns/getTime");
+const parseISO = require("date-fns/parseISO");
+const argv = require("minimist")(process.argv.slice(2));
 
 const getArg = (short, long) => {
-  if (argv[short] != null) return argv[short]
-  if (argv[long] != null) return argv[long]
-  return null
-}
+  if (argv[short] != null) return argv[short];
+  if (argv[long] != null) return argv[long];
+  return null;
+};
 
 const getTeam = (comp, homeAway) =>
   comp.competitions[0].competitors.find(
     competitor => competitor.homeAway === homeAway
-  )
+  );
 const isRanked = team =>
-  team.curatedRank.current !== 99 && team.curatedRank.current !== 0
+  team.curatedRank.current !== 99 && team.curatedRank.current !== 0;
 
 const groupByStartDate = arr => {
   return arr.reduce((acc, val, i) => {
-    const startDate = format(val.competitions[0].startDate, 'YYYYMMDD')
-    acc[startDate] = (acc[startDate] || []).concat(arr[i])
-    return acc
-  }, {})
-}
+    const startDate = format(
+      parseISO(val.competitions[0].startDate),
+      "yyyyMMdd"
+    );
+    acc[startDate] = (acc[startDate] || []).concat(arr[i]);
+    return acc;
+  }, {});
+};
 
 const sortRankVsNotRanked = events => {
   // sort games by highest ranked team participating 1 -> 25
   const ranked = events
     .filter(event => {
-      return event.competitions[0].competitors.some(team => isRanked(team))
+      return event.competitions[0].competitors.some(team => isRanked(team));
     })
     .sort((a, b) => {
       const highestRank = x => {
-        const home = getTeam(x, 'home')
-        const away = getTeam(x, 'away')
+        const home = getTeam(x, "home");
+        const away = getTeam(x, "away");
         if (isRanked(home) && isRanked(away)) {
           return Math.min(
             home.curatedRank.current,
             away.curatedRank.current
-          ).toString()
+          ).toString();
         }
         return isRanked(home)
           ? home.curatedRank.current
-          : away.curatedRank.current
-      }
-      return highestRank(a) - highestRank(b)
-    })
+          : away.curatedRank.current;
+      };
+      return highestRank(a) - highestRank(b);
+    });
 
   // sort non-ranked games by start time
   const notRanked = events
     .filter(event => {
       return event.competitions[0].competitors.every(
         team => team.curatedRank.current === 99
-      )
+      );
     })
     .sort((a, b) => {
       return (
-        getTime(a.competitions[0].startDate) -
-        getTime(b.competitions[0].startDate)
-      )
-    })
+        getTime(parseISO(a.competitions[0].startDate)) -
+        getTime(parseISO(b.competitions[0].startDate))
+      );
+    });
 
-  return [...ranked, ...notRanked]
-}
+  return [...ranked, ...notRanked];
+};
 
 const sortEvents = eventList => {
-  const groupedByDate = groupByStartDate(eventList)
+  const groupedByDate = groupByStartDate(eventList);
   return [].concat(
     ...Object.keys(groupedByDate).map(eventDay =>
       sortRankVsNotRanked(groupedByDate[eventDay])
     )
-  )
-}
+  );
+};
 
 const get = async () => {
   try {
-    const req = await axios.get(reqUrl)
-    const events = req.data.events
+    const req = await axios.get(reqUrl);
+    const events = req.data.events;
 
     const scraped = sortEvents(events).reduce((prev, curr) => {
-      const comp = curr.competitions[0]
-      const home = getTeam(curr, 'home')
-      const away = getTeam(curr, 'away')
-      const date = format(comp.startDate, 'ddd, MMM D')
+      const comp = curr.competitions[0];
+      const home = getTeam(curr, "home");
+      const away = getTeam(curr, "away");
+      const date = format(parseISO(comp.startDate), "E, MMM d");
       const time =
-        comp.status.type.detail === 'Postponed'
-          ? 'Postponed'
-          : format(new Date(comp.startDate), 'h:mm A')
-      const final = comp.status.type.completed
-      const isConference = curr.competitions[0].conferenceCompetition
-      const isNeutral = curr.competitions[0].neutralSite
+        comp.status.type.detail === "Postponed"
+          ? "Postponed"
+          : format(parseISO(comp.startDate), "h:mm a");
+      const final = comp.status.type.completed;
+      const isConference = curr.competitions[0].conferenceCompetition;
+      const isNeutral = curr.competitions[0].neutralSite;
 
       const thisGame = [
         date,
         time,
         away.team.location,
-        away.score === '0' || !final ? ' ' : away.score,
+        away.score === "0" || !final ? " " : away.score,
         home.team.location,
-        home.score === '0' || !final ? ' ' : home.score,
+        home.score === "0" || !final ? " " : home.score,
         isConference,
-        isNeutral,
-      ]
+        isNeutral
+      ];
 
-      return `${prev}\n${thisGame.join('+')}`
-    }, '')
-    console.log(scraped)
+      return `${prev}\n${thisGame.join("+")}`;
+    }, "");
+    console.log(scraped);
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
-}
+};
 
-let reqUrl
-let dateArg = getArg('d', 'date')
-let weekArg = getArg('w', 'week')
+let reqUrl;
+let dateArg = getArg("d", "date");
+let weekArg = getArg("w", "week");
 
 if (dateArg != null && weekArg != null) {
-  console.error("Don't use both week and date flags together")
-  return
+  console.error("Don't use both week and date flags together");
+  return;
 }
 
 if (dateArg != null) {
-  reqUrl = `http://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/scoreboard?lang=en&region=us&calendartype=blacklist&limit=500&dates=${dateArg.toString()}&tz=America/New_York&groups=50`
+  reqUrl = `http://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/scoreboard?lang=en&region=us&calendartype=blacklist&limit=500&dates=${dateArg.toString()}&tz=America/New_York&groups=50`;
 }
 
 if (weekArg != null) {
-  reqUrl = `http://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/scoreboard?lang=en&region=us&calendartype=blacklist&limit=500&dates=2019&tz=America/New_York&groups=50&week=${weekArg.toString()}`
+  reqUrl = `http://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/scoreboard?lang=en&region=us&calendartype=blacklist&limit=500&dates=2020&tz=America/New_York&groups=50&week=${weekArg.toString()}`;
 }
 
-get()
+get();
